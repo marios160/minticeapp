3c394239425893f249df52414970e3b3096e9e23
Mateusz Błaszczak <marios160@o2.pl>
2021-03-19 22:18:57 +0100

    Init

---
+++ MintIceApp/Views/RecipesPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using MintIceApp.Models;
+using MintIceApp.Views;
+using MintIceApp.ViewModels;
+
+namespace MintIceApp.Views
+{
+    public partial class RecipesPage : ContentPage
+    {
+        RecipesViewModel _viewModel;
+
+        public RecipesPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new RecipesViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
---
+++ MintIceApp/Views/SummaryPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using MintIceApp.Models;
+using MintIceApp.Views;
+using MintIceApp.ViewModels;
+
+namespace MintIceApp.Views
+{
+    public partial class SummaryPage : ContentPage
+    {
+        SummaryViewModel _viewModel;
+
+        public SummaryPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new SummaryViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
